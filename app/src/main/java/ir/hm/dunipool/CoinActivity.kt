e5e552b9a52b93fb91e93f8549ba55baa6c35660
ASUS <raminhm605@gmail.com>
2022-05-21 12:29:12 +0430

    about init layout Coin

---
+++ app/src/main/java/ir/hm/dunipool/CoinActivity.kt
@@ -0,0 +1,11 @@
+package ir.hm.dunipool
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CoinActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coin)
+    }
+}
